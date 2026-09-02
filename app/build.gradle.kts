3507bef2e0441047003f3c5e0fe797d434e16063
advocate4u <advocatemittal4u@gmail.com>
2026-09-02 20:12:39 +0530

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,17 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "com.example.kbawelfaremessenger"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.kbawelfaremessenger"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
