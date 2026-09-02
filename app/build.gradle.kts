2eb3c7576a5e70a3b8136f28a5e8034327269901
advocate4u <advocatemittal4u@gmail.com>
2026-09-02 21:54:52 +0530

    Update app version and add RecyclerView dependency
    Updated versionCode and versionName in defaultConfig. Added RecyclerView dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.kbawelfaremessenger"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     compileOptions {
@@ -30,4 +30,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.work:work-runtime-ktx:2.10.1")
+
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 }
