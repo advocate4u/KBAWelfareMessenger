e9cc3542b387b2ddc184b0eb7de0331dc2be5c6d
advocate4u <advocatemittal4u@gmail.com>
2026-09-04 03:29:54 +0530

    Fix invalid BuildConfig string syntax

--- licensemanager/build.gradle.kts
+++ licensemanager/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         release {
             isMinifyEnabled = false
             signingConfig = signingConfigs.getByName("release")
-            buildConfigField("String", "MYADV_SIGNING_PRIVATE_KEY_B64", signingPrivateKey.replace("\\", "\\\\").replace(""", "\\"").let { ""$it"" })
+            buildConfigField("String", "MYADV_SIGNING_PRIVATE_KEY_B64", ""$signingPrivateKey"")
         }
     }
 
